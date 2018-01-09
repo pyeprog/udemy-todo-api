563530aa193bec924c224e76b766966b3c4b630e
pd <pd@pds-MacBook-Pro.local>
2018-01-09 16:36:19 +0800

    2018/1/9:Add user schema and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,16 +1,64 @@
+const validator = require('validator');
 const mongoose = require('mongoose');
-var User = mongoose.model('User', {
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+
+var UserSchema = new mongoose.Schema({
   name: {
     type: String,
     default: null
   },
   email: {
+    unique: true,
     type: String,
-    require: true,
+    required: true,
     minlength: 1,
-    trim: true
-  }
+    trim: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
+  
 });
+
+UserSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+  user.tokens.push({access, token});
+
+  return user.save().then(() => {
+    return token;
+  }).then((token) => {
+
+  });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {
   User
--- server/server.js
+++ server/server.js
@@ -89,6 +89,20 @@ app.patch('/todos/:id', (req, res) => {
   
 });
 
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then((user) => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 })
