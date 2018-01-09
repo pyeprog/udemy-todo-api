2463b071ad4287f150af558dfafe016a5299f337
pd <pd@pds-MacBook-Pro.local>
2018-01-09 18:53:31 +0800

    2018/1/9:Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose.js');
 const {Todo} = require('./models/todo.js');
 const {User} = require('./models/user.js');
+const {authenticate}= require('./middleware/authenticate');
 
 var app = express();
 
@@ -101,6 +102,10 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(3000, () => {
